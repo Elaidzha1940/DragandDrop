daa7a9d42f1ac1c1caf018bc0a47e126876e401b
Elaidzha1940 <eli080293@gmail.com>
2023-11-01 22:31:42 +0300

    DragandDrop

---
+++ DragandDrop/DragandDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragandDropApp.swift
+//  DragandDrop
+//
+//  Created by Elaidzha Shchukin on 01.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct DragandDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
