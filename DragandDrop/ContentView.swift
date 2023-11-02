9c6216fb0c00bc95b96c1ce94a07879be08c796e
Elaidzha1940 <eli080293@gmail.com>
2023-11-02 19:44:49 +0300

    Done

--- DragandDrop/ContentView.swift
+++ DragandDrop/ContentView.swift
@@ -12,7 +12,7 @@ import Algorithms
 import UniformTypeIdentifiers
 
 struct ContentView: View {
-    @State private var toDoTasks: [DeveloperTask] = ["Black Coffee", "Claptone", "Loui Vegga", "Bedouin"]
+    @State private var toDoTasks: [DeveloperTask] = [MockData.taskOne, MockData.taskTwo, MockData.taskThree, MockData.taskFour, MockData.taskFive]
     @State private var inProgressTask: [DeveloperTask] = []
     @State private var doneTask: [DeveloperTask] = []
     
@@ -24,10 +24,10 @@ struct ContentView: View {
         
         HStack(spacing: 10) {
             ListView(title: "To Do", tasks: toDoTasks, isTargeted: isToDoTargeted)
-                .dropDestination(for: String.self) { droppedTasks, location in
+                .dropDestination(for: DeveloperTask.self) { droppedTasks, location in
                     for task in droppedTasks {
-                        inProgressTask.removeAll { $0 == task }
-                        doneTask.removeAll { $0 == task }
+                        inProgressTask.removeAll { $0.id == task.id }
+                        doneTask.removeAll { $0.id == task.id }
                     }
                     let totalTasks = toDoTasks + droppedTasks
                     toDoTasks = Array(totalTasks.uniqued())
@@ -37,10 +37,10 @@ struct ContentView: View {
                 }
             
             ListView(title: "In Progress", tasks: inProgressTask, isTargeted: isInProgressTargeted)
-                .dropDestination(for: String.self) { droppedTasks, location in
+                .dropDestination(for: DeveloperTask.self) { droppedTasks, location in
                     for task in droppedTasks {
-                        toDoTasks.removeAll { $0 == task }
-                        doneTask.removeAll { $0 == task }
+                        toDoTasks.removeAll { $0.id == task.id }
+                        doneTask.removeAll { $0.id == task.id }
                     }
                     let totalTasks = inProgressTask + droppedTasks
                     inProgressTask = Array(totalTasks.uniqued())
@@ -50,10 +50,10 @@ struct ContentView: View {
                 }
     
             ListView(title: "Done", tasks: doneTask, isTargeted: isDoneTargeted)
-            .dropDestination(for: String.self) { droppedTasks, location in
+            .dropDestination(for: DeveloperTask.self) { droppedTasks, location in
                     for task in droppedTasks {
-                        toDoTasks.removeAll { $0 == task }
-                        inProgressTask.removeAll { $0 == task }
+                        toDoTasks.removeAll { $0.id == task.id }
+                        inProgressTask.removeAll { $0.id == task.id }
                     }
                     let totalTasks = doneTask + droppedTasks
                     doneTask = Array(totalTasks.uniqued())
@@ -73,7 +73,7 @@ struct ContentView: View {
 
 struct ListView: View {
     let title: String
-    let tasks: [String]
+    let tasks: [DeveloperTask]
     let isTargeted: Bool
     
     var body: some View {
@@ -87,8 +87,8 @@ struct ListView: View {
                     .foregroundColor(isTargeted ? Color.mint.opacity(0.5) : Color.gray.opacity(0.7))
                 
                 VStack(alignment: .leading, spacing: 10) {
-                    ForEach(tasks, id: \.self) { task in
-                        Text(task)
+                    ForEach(tasks, id: \.id) { task in
+                        Text(task.title)
                             .foregroundColor(.white)
                             .padding(10)
                             .background(Color.black.opacity(0.5))
@@ -102,7 +102,7 @@ struct ListView: View {
     }
 }
 
-struct DeveloperTask: Codable, Transferable {
+struct DeveloperTask: Codable, Hashable, Transferable {
     let id: UUID
     let title: String
     let owner: String
@@ -124,12 +124,22 @@ struct MockData {
                                        note: "Note")
     
     static let taskTwo = DeveloperTask(id: UUID(),
-                                       title: "Black Coffee",
+                                       title: "Loui Vega",
                                        owner: "ElaidzhaShchukin",
                                        note: "Note")
     
     static let taskThree = DeveloperTask(id: UUID(),
-                                       title: "Black Coffee",
+                                       title: "Bedouin",
+                                       owner: "ElaidzhaShchukin",
+                                       note: "Note")
+    
+    static let taskFour = DeveloperTask(id: UUID(),
+                                       title: "Claptone",
+                                       owner: "ElaidzhaShchukin",
+                                       note: "Note")
+    
+    static let taskFive = DeveloperTask(id: UUID(),
+                                       title: "Dunmore Brothers",
                                        owner: "ElaidzhaShchukin",
                                        note: "Note")
 }
